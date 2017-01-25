f7732965b416809a611ba71a0e0face712978409
Charles Dunn <dunnc1551@gmail.com>
2017-01-24 21:13:30 -0700

    Add name attribute to input fields and buttons. Add tests to feature-test.js

--- test/feature-test.js
+++ test/feature-test.js
@@ -7,11 +7,33 @@ describe('testing 2Dobox', function() {
   .forBrowser('chrome')
   .build();
 
-  test.it.only('should allow me to add a title and description', function() {
+  test.it('should allow me to add a title and task', function() {
     this.timeout(10000);
     driver.get('http://localhost:8080');
 
+    const title = driver.findElement({name: 'title'});
+    title.sendKeys('Title tester')
 
+    title.getAttribute('value').then((value) => {
+      assert.equal(value, 'Title tester');
+    });
+
+    const task = driver.findElement({name: 'task'});
+    task.sendKeys('task tester')
+
+    task.getAttribute('value').then((value)=> {
+      assert.equal(value, 'task tester');
+    });
+  });
+
+  test.it('save button should add a new task to the page', ()=> {
+    this.timeout(10000);
+    const button = driver.findElement({name: 'save'});
+    button.click();
+
+    driver.findElements({tagName: 'ul'}).then((ul)=> {
+      assert.equal(ul.length, 1);
     });
     driver.quit();
+  });
  });
